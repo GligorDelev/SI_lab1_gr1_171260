755844311503d74e5b0c490b8353fb67aa79faf5
Gligor Delev <gligordelev@gmail.com>
2020-04-12 22:29:40 +0200

    student class finished

--- Student.java
+++ Student.java
@@ -5,14 +5,30 @@ class Student {
 	int labPoints[];
 
 	//TODO constructor
-
+	public Student(){
+		
+	}
 	//TODO seters & getters
+	public void setIndex(String index){this.index=index;}
+	public void setFirstName(String firstName){this.firstName=firstName;}
+	public void setLastName(String lastName){this.lastName=lastName;}
+	public String getIndex(){return index;}
+	public String getFirstName(){return firstName;}
+	public String getLastName(){return lastName;}
+	public void setLabPoints(int [] labPoints){this.labPoints=labPoints;}
+	public int [] getLabPoints(){return labPoints;}
 
 	public double getAverage() {
 		//TODO
+		int sum=0;
+		foreach(int ocena:labPoints){
+			sum+=ocena;	
+		}
+		return sum/labPoints.length;
 	}
 
 	public boolean hasSignature() {
 		//TODO
+		return labPoints.length>8;
 	}
 }
